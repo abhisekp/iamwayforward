61a48d82215d1fb58266fdd529402f21c378dd0f
abhisekp <abhisekp@engineer.com>
2020-07-25 16:21:12 +0000

    Deploying to gh-pages from  @ 02b362c7817ed10ebfc1f8905603033c8927c79f 🚀

---
+++ precache-manifest.76eb97f2bbdcb0f197c0c95af548d6f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acb85b772a2006c781d115765b9252d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a654a8c43b06345a65c",
+    "url": "/static/css/main.2a341620.chunk.css"
+  },
+  {
+    "revision": "2305797b82233b16efd1",
+    "url": "/static/js/2.39ded67b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.39ded67b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a654a8c43b06345a65c",
+    "url": "/static/js/main.2cb8b7b9.chunk.js"
+  },
+  {
+    "revision": "c64e9f23ced049fad3de",
+    "url": "/static/js/runtime-main.0d3cb287.js"
+  },
+  {
+    "revision": "a985bf9024eb15544bceb4180cd4ad23",
+    "url": "/static/media/iamwayforward_logo_landscape_lowres.a985bf90.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.77f0f40425f43793a72c200e53446da5.js"
+  "/precache-manifest.76eb97f2bbdcb0f197c0c95af548d6f3.js"
 );
 
 self.addEventListener('message', (event) => {
