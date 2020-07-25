070af20053fcdfff9610b083f7cb3068d4a80e53
Abhisek Pattnaik <abhisekp@engineer.com>
2020-07-25 18:43:37 +0530

    feat(ui): Setup website

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="App-container">
+          <h1>
+            <p className="App-title">#iamwayforward <span role="img" aria-label="go forward">⏩</span><br />
+              <span className="App-subtitle"><em>Let's become successful together</em></span>
+            </p>
+          </h1>
+          <p className="App-content">We provide a unique ecosystem of <strong><em>guided Entrepreneurship</em></strong> to help you become an Entrepreneur. <br />
+            <em>Fill-in the form below if you're interested in <strong>Mentorship</strong> based Partnership.</em> <br />
+            <span role="img" aria-label="scroll down">👇 👇 👇</span>
+          </p>
+        </div>
       </header>
+
+      <iframe title="Business Partnership & Mentorship Interest Form" src="https://docs.google.com/forms/d/e/1FAIpQLScnfXE1hXTwfho5MRzHomQD4MXHO7mJOACUGpUYsVVI3xTbVg/viewform?embedded=true" width="640" height="100%" frameBorder="0" marginHeight="0" marginWidth="0">Loading…</iframe>
     </div>
   );
 }
