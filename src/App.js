39d5c4fcd5aebe9605d47fdc57785aef00d420b1
Abhisek Pattnaik <abhisekp@engineer.com>
2020-07-25 20:46:29 +0530

    fix(ui): Set form iframe width to 100%

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
         </div>
       </header>
 
-      <iframe title="Business Partnership & Mentorship Interest Form" src="https://docs.google.com/forms/d/e/1FAIpQLScnfXE1hXTwfho5MRzHomQD4MXHO7mJOACUGpUYsVVI3xTbVg/viewform?embedded=true" width="640" height="100%" frameBorder="0" marginHeight="0" marginWidth="0">Loading…</iframe>
+      <iframe title="Business Partnership & Mentorship Interest Form" src="https://docs.google.com/forms/d/e/1FAIpQLScnfXE1hXTwfho5MRzHomQD4MXHO7mJOACUGpUYsVVI3xTbVg/viewform?embedded=true" width="100%" height="100%" frameBorder="0" marginHeight="0" marginWidth="0">Loading…</iframe>
     </div>
   );
 }
