10051745527c1180e177a0933cbab8f3301edc89
Abhisek Pattnaik <abhisekp@engineer.com>
2020-07-25 07:57:10 +0530

    Updates

---
+++ precache-manifest.e8b12c73a67b31ee62cf6b5080b5b0b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eedb7efdee1b9a7d3e2907e5a2fe80dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e6dcedc84497f050693",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7eb6d1f47f24f8171af6",
+    "url": "/static/js/2.0266157f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0266157f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e6dcedc84497f050693",
+    "url": "/static/js/main.93896a60.chunk.js"
+  },
+  {
+    "revision": "055d880906494aba4e2b",
+    "url": "/static/js/runtime-main.021393f3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
